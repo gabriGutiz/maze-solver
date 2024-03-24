f22aaca4fd70004f6e3681bf31b785324876f84e
Gabriel Gutierrez <gabri.gutiz@gmail.com>
2024-03-24 17:12:13 -0300

    feat: add BredthFirst solver

---
+++ MazeSolver/Solver/BreadthFirst.py
@@ -0,0 +1,30 @@
+
+from MazeSolver.Maze import Maze
+from MazeSolver.Tree import Tree
+from MazeSolver.Solver.SolverBase import SolverBase
+
+
+class BreadthFirst(SolverBase):
+
+    def __init__(self, maze: Maze):
+       SolverBase.__init__(self, maze) 
+
+
+    def solve(self, nodes: list[Tree] = None) -> Tree:
+        if not nodes:
+            nodes = [Tree(self.maze.start_position)]
+
+        new_nodes = []
+
+        for node in nodes:
+            possible_states = self._get_possible_paths(node.state)
+
+            for possible_state in possible_states:
+                if self.maze.get_coordinates_type(possible_state[0], possible_state[1]) == self.maze.end:
+                    return node.new_state(possible_state)
+                if possible_state in node.get_positions():
+                    continue
+
+                new_nodes.append(node.new_state(possible_state))
+
+        return self.solve(new_nodes)
--- MazeSolver/__init__.py
+++ MazeSolver/__init__.py
@@ -1,2 +1,3 @@
 
 from .Maze import Maze
+from .Solver.BreadthFirst import BreadthFirst
